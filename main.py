5be44b151f1f7e8e1e7f6bb08c67374e605cd30b
Hattori Shunya <hattoris@revolvesys.co.jp>
2025-10-20 16:58:42 +0900

    added output parsing

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ load_dotenv()
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
-from langchain_aws import ChatBedrock
-from langchain_aws import ChatBedrockConverse
+from langchain_aws import ChatBedrock, ChatBedrockConverse
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
 
@@ -22,14 +27,23 @@ llm = ChatBedrockConverse(
     # other params...
 )
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
+
 agent = create_react_agent(
-    llm = llm,
-    tools = tools,
-    prompt = react_prompt,
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions,
 )
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
 
+chain = agent_executor | extract_output | parse_output
 
 
 def main():
